6ed19045db224a3ec8080fe754819c35c412433b
Alvaro <alvaronrs@hotmail.com>
2024-09-10 18:33:03 -0300

    Início do header

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -1,12 +1,16 @@
 import { Link } from 'react-router-dom';
+import logoJogo from 'assets/Logo.png';
 import styles from './Menu.module.css';
 
 export default function Menu(){
     return(
         <header>
+            <img src={logoJogo} id={styles.logoImg} alt="Logo do Jogo Intercessores" />
+            <hr/>
             <nav>
                 <Link to="/">Início</Link>
-                <Link to="/sobre">Sobre Mim</Link>
+                <Link to="/">Como Jogar</Link>
+                <Link to="/sobre">Sobre o criador</Link>
             </nav>
         </header>
     );
