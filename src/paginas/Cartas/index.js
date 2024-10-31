6a7ccaed64dd5df331e39587cba4421b366a9276
Alvaro <alvaronrs@hotmail.com>
2024-10-31 09:47:01 -0300

    Aplicação de Imagens mais leves para toda a página

--- src/paginas/Cartas/index.js
+++ src/paginas/Cartas/index.js
@@ -1,13 +1,16 @@
 import Titulo from "componentes/Titulo";
 import AreaConteudo from "paginas/AreaConteudo";
 import baseDadosCartas from "../../cards.json";
+import baseCartasVirtudesTeologais from "../../virtueCards.json";
 import ConjuntoCartas from "componentes/ConjuntoCartas";
 
 export default function Cartas(){
     return(
         <section className="text-center">
             <AreaConteudo paddingY={"py-3"} paddingX={"px-0"} tamanhoFonte={"fs-4"} corFonte={"fc-laranja"} corFundo={"bgc-creme"}>
-                <Titulo>Cartas do Jogo</Titulo>
+                <Titulo corFonte={"fc-marrom"}>Cartas de Virtudes Teologais</Titulo>
+                <ConjuntoCartas dados={baseCartasVirtudesTeologais} />
+                <Titulo corFonte={"fc-marrom"}>Cartas de Santos</Titulo>
                 <ConjuntoCartas dados={baseDadosCartas} />
             </AreaConteudo>
         </section>
